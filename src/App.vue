b3c1c32bee7d25bef2b1d11e8e28e7630c407f98
AlessandrodeAlmeida2 <agostini55@gmail.com>
2024-01-22 13:34:50 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,45 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  {{ user.first_name }} {{ user.last_name }}
+  <br>
+  {{ admin.first_name }} {{ admin.last_name }}
+  <img @click="changeName()" alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
+import { reactive, ref } from 'vue'
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  setup() {
+    const user = reactive({
+      first_name: 'Jhon',
+      last_name: 'Snow'
+    })
+
+    const admin = ref({
+      first_name: 'Master',
+      last_name: 'User'
+    })
+
+    let name = 'Alessandro'
+
+    const changeName = () => {
+      alert('chegou')
+      name = 'Jhon Snow'
+      user.first_name = 'Sansa'
+      admin.value.first_name = 'Main' // usar .value para reatividade
+    }
+    return {
+      user,
+      name,
+      admin,
+      changeName
+    }
   }
 }
 </script>
